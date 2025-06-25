67ed3e508541e1ee4657d3f8c6ab0d17de5671af
Henry <henryapanisile@gmail.com>
2025-06-25 11:04:55 +0100

    Initial commit

--- app/src/main/java/com/example/re_estate/activity/PaymentMethodScreen.java
+++ app/src/main/java/com/example/re_estate/activity/PaymentMethodScreen.java
@@ -1,26 +1,40 @@
 package com.example.re_estate.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.SystemBarStyle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.re_estate.R;
+import com.example.re_estate.activity.payment.AddCardScreen;
+import com.example.re_estate.databinding.ActivityPaymentMethodScreenBinding;
 
 public class PaymentMethodScreen extends AppCompatActivity {
+    ActivityPaymentMethodScreenBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_payment_method_screen);
+        EdgeToEdge.enable(this, SystemBarStyle.light(
+                getColor(R.color.white), getColor(R.color.white)
+        ));
+        binding = ActivityPaymentMethodScreenBinding.inflate(getLayoutInflater());
+        setContentView(binding.main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            v.setPadding(32, systemBars.top, 32, 0);
             return insets;
+        });
+
+        binding.addCard.setOnClickListener(v -> {
+            Intent intent = new Intent(this, AddCardScreen.class);
+            intent.putExtra("type", "Add Card");
+            startActivity(intent);
         });
     }
 }
